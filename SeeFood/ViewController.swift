1bf191894e7a65c29705fe189d1e65dc6d1e1acc
Nikolai Saganenko <9952728@gmail.com>
2021-10-13 15:42:51 +0300

    Set basic UI Add InceptionV3 ML model

--- SeeFood/ViewController.swift
+++ SeeFood/ViewController.swift
@@ -6,14 +6,56 @@
 //
 
 import UIKit
+import CoreML
+import Vision
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UINavigationControllerDelegate, UIImagePickerControllerDelegate {
+
+    @IBOutlet weak var imageView: UIImageView!
+    
+    let imagePicker = UIImagePickerController()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        imagePicker.delegate = self
+        imagePicker.sourceType = .photoLibrary
+        imagePicker.allowsEditing = false
+        
+    }
+    
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        
+        if let userPickedImage = info[UIImagePickerController.InfoKey.originalImage] as? UIImage {
+            imageView.image = userPickedImage
+            
+            guard let ciImage = CIImage(image: userPickedImage) else {
+                fatalError("Can't convert UIImage into CIImage")
+            }
+
+        }
+        
+        imagePicker.dismiss(animated: true, completion: nil)
+
+    }
+    
+    func detect(image: CIImage){
+        
+        guard let model = try? VNCoreMLModel(for: Inceptionv3().model) else {
+            fatalError("Loading CoreML model failed")
+        }
+        
+        let request = VNCoreMLRequest(model: model) { (request, error) in
+            <#code#>
+        }
     }
 
+    @IBAction func cameraButtonPressed(_ sender: UIBarButtonItem) {
+        
+        present(imagePicker, animated: true, completion: nil)
+        
+    }
 
 }
 
